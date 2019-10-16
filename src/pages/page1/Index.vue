f0f87bffe94445d7d04483c90be112d0cb3dd937
dq <251646075@qq.com>
2019-10-16 14:35:52 +0800

    自定义上传文件样式

--- src/pages/page1/Index.vue
+++ src/pages/page1/Index.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="page_wrap">这是页面1</div>
+  <div>
+    <div class="upload-file__wrap">
+      <input type="file" ref="file" accept style="display: none" v-on:change="onFileChange"/>
+      <div class="upload-select" @click.stop="selectFileHandle">
+        <span class="icon-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+        <span>upload</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
-      return {}
+      return {
+        file: null
+      }
+    },
+    methods: {
+      selectFileHandle () {
+        this.$refs.file.click()
+      },
+      onFileChange (event) {
+        console.log(event)
+      }
     }
   }
 </script>
 
 <style scoped lang="less">
-  .page_wrap {
+  .upload-file__wrap {
     display: flex;
     flex: 1;
-    color: #fff;
-    font-weight: bold;
-    align-items: center;
+    margin: 50px 0 0 0;
     justify-content: center;
-    background: url("/static/img/page_602809.jpg");
-    background-size: 100% 100%;
+
+    & .upload-select {
+      display: flex;
+      width: 180px;
+      height: 180px;
+      border: 1.5px dashed #ccc;
+      border-radius: 8px;
+      color: #888;
+      background: #fafafa;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+      & .icon-plus {
+        font-size: 40px;
+      }
+
+      &:active {
+        background: #ff0000;
+      }
+    }
   }
 </style>
