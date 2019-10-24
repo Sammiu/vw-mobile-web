a5d68a7f34d2bd351cb0af288d7ac28d66b5e26e
dq <251646075@qq.com>
2019-10-24 17:36:14 +0800

    1. 增加 koa http proxy middle

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = new Koa()
 const fs = require('fs')
 const path = require('path')
 const favicon = require('koa-favicon')
+const proxy = require('koa-server-http-proxy')
 const {createBundleRenderer} = require('vue-server-renderer')
-/** 假设已经实现 */
 const setupDevServer = require('./build/ssr/setup-dev-server')
+const proxyTable = require('./config').dev.proxyTable
 
 const resolve = file => path.resolve(__dirname, file)
 
@@ -21,14 +22,14 @@ const createBundle = (bundle, clientManifest, template) => {
 let renderer
 const templatePath = path.resolve(__dirname, './index.template.html')
 const template = fs.readFileSync(templatePath, 'utf-8')
-if (process.env.NODE_ENV === 'production') {
+if (global.process.env.NODE_ENV === 'production') {
   /** 生成服务端渲染函数 */
   const serverBundle = require('./dist/vue-ssr-server-bundle.json')
   const clientManifest = require('./dist/vue-ssr-client-manifest.json')
   renderer = createBundle(serverBundle, clientManifest, template)
   app.use(require('koa-static')(resolve('./dist')))
 } else {
-  renderer = setupDevServer(app, templatePath, (bundle, clientManifest) => {
+  setupDevServer(app, templatePath, (bundle, clientManifest) => {
     renderer = createBundle(bundle, clientManifest, template)
   })
 }
@@ -42,11 +43,24 @@ function renderToString (context) {
 }
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')))
+
+/** http proxy middle */
+Object.keys(proxyTable).forEach((context) => {
+  const options = proxyTable[context]
+  app.use(proxy(context, options))
+})
+
 /** response */
 app.use(async (ctx, next) => {
   try {
+    for (let proxyUrl in proxyTable) {
+      if (ctx.url.startsWith(proxyUrl)) {
+        return next()
+      }
+    }
+    
     /** {{title}} 替换template占位符 */
-    const context = {title: '服务端渲染测试', url: ctx.url}
+    const context = {title: '服务端渲染测试', url: ctx.url, cookies: ctx.cookies.request.headers.cookie}
     
     /** 将服务器端渲染好的html返回给客户端 */
     ctx.body = await renderToString(context)
@@ -56,7 +70,7 @@ app.use(async (ctx, next) => {
     ctx.set('Server', 'Koa2 server side render')
   } catch (e) {
     /** 如果没找到，放过请求，继续运行后面的中间件 */
-    next()
+    return next()
   }
 })
 
