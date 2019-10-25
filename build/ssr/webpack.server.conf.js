dd220ac1ad8f8ffb1b7e03dcc96a67259b5e64bf
dq <251646075@qq.com>
2019-10-25 13:49:10 +0800

    修复SSR在重复打包CSS的bug

--- build/ssr/webpack.server.conf.js
+++ build/ssr/webpack.server.conf.js
@@ -3,9 +3,9 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const nodeExternals = require('webpack-node-externals')
 const baseConfig = require('../webpack.base.conf.js')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
-// 去除打包css的配置
-baseConfig.module.rules[1].options = ''
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = merge(baseConfig, {
   // 将 entry 指向应用程序的 server entry 文件
@@ -35,6 +35,7 @@ module.exports = merge(baseConfig, {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
     }),
+    ...isProduction ? [new ExtractTextPlugin({filename: 'common.[chunkhash].css'})] : [],
     // 这是将服务器的整个输出
     // 构建为单个 JSON 文件的插件。
     // 默认文件名为 `vue-ssr-server-bundle.json`
