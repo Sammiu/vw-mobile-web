105ed168b11a82dd6889018f77636d5273ad2d32
dq <251646075@qq.com>
2019-12-23 11:45:27 +0800

    1. 修复生产模式bug

--- build/ssr/webpack.server.conf.js
+++ build/ssr/webpack.server.conf.js
@@ -34,7 +34,7 @@ module.exports = merge(baseConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      'process.env.VUE_ENV': 'server'
+      'process.env.VUE_ENV': '"server"'
     }),
     ...isProduction ? [new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
