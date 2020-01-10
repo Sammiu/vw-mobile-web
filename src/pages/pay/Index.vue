ea5e7d7260d56598e47b88fdd19cb3c4e1ea8d25
sammiu <251646075@qq.com>
2020-01-10 21:09:59 +0800

    移除以前的测试页面，增加支付页面

---
+++ src/pages/pay/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="less">
+.container {
+  width: 100%;
+  height: 300px;
+  background-color: #ff0000;
+}
+</style>
