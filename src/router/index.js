0515187e15e695f853bfd647707ae8642e7bed74
sammiu <251646075@qq.com>
2020-01-10 21:10:40 +0800

    路由的跳转方式的映射数据结构调整

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,47 @@
 import Vue from 'vue'
 import store from '@/store'
 import Router from 'vue-router'
-import {UPDATE_DIRECTION} from '@/store/types'
+import { UPDATE_DIRECTION } from '@/store/types'
 import Main from '@/pages/main/Index'
 
 const Login = () => import('@/pages/login/Index')
-const Page1 = () => import('@/pages/page1/Index')
-const Page2 = () => import('@/pages/page2/Index')
-const Page3 = () => import('@/pages/page3/Index')
-const Page4 = () => import('@/pages/page4/Index')
+const Pay = () => import('@/pages/pay/Index')
 
 Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
   routes: [
-    {path: '/', name: 'main', component: Main},
-    {path: '/login', name: 'login', meta: {requireAuth: true}, component: Login},
-    {path: '/page1', name: 'Page1', meta: {requireAuth: true}, component: Page1},
-    {path: '/page2', name: 'Page2', meta: {requireAuth: true}, component: Page2},
-    {path: '/page3', name: 'Page3', meta: {requireAuth: true}, component: Page3},
-    {path: '/page4', name: 'Page4', meta: {requireAuth: true}, component: Page4}
+    { path: '/', name: 'main', component: Main },
+    { path: '/login', name: 'login', meta: { requireAuth: true }, component: Login },
+    { path: '/pay', name: 'Pay', meta: { requireAuth: true }, component: Pay }
   ]
 })
 
 let pageStack = null
 
 if (process.env.VUE_ENV === 'client') {
-  pageStack = {'/': {form: '/login', direction: 'forward'}}
+  pageStack = {
+    '/': [
+      { form: '/login', direction: 'forward' },
+      { form: '/pay', direction: 'reverse' }
+    ],
+    '/pay': [
+      { form: '/', direction: 'forward' }
+    ]
+  }
 }
 
 router.beforeEach(function (to, from, next) {
   if (pageStack && pageStack.hasOwnProperty(to.path)) {
-    const item = pageStack[to.path]
-    store.commit(UPDATE_DIRECTION, {direction: item.form === from.path ? item.direction : ''})
+    const item = pageStack[to.path].find(o => o.form === from.path)
+    if (item) {
+      store.commit(UPDATE_DIRECTION, { direction: item.direction || '' })
+    } else {
+      store.commit(UPDATE_DIRECTION, { direction: '' })
+    }
   } else {
-    store.commit(UPDATE_DIRECTION, {direction: ''})
+    store.commit(UPDATE_DIRECTION, { direction: '' })
   }
   if (/\/http/.test(to.path)) {
     let url = to.path.split('http')[1]
